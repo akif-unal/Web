f5e410618e61c1571900508ea7dcbff2660a6f47
akifunal <akifunal05@gmail.com>
2020-10-21 17:50:50 +0300

    backdrop and modal added with js

--- Udemy/CSS - The Complete Guide/Project/shared.js
+++ Udemy/CSS - The Complete Guide/Project/shared.js
@@ -6,26 +6,40 @@ const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 
 // console.dir(backdrop);
+// console.dir(backdrop.style);
+
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 const closeModal = () => {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  // modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open');
+  };
+  backdrop.classList.remove('open');
 }
 
-modalNoButton.addEventListener('click', closeModal)
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
 
 backdrop.addEventListener('click', () => {
   closeModal();
-  mobileNav.style.display = 'none';
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
 })
 
 toggleButton.addEventListener('click', () => {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 })
