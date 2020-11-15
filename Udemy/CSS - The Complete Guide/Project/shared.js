eb8c4c948209c64376488f6c562354392b64ede2
akifunal <akifunal05@gmail.com>
2020-11-15 23:17:35 +0300

    animation event listeners added

--- Udemy/CSS - The Complete Guide/Project/shared.js
+++ Udemy/CSS - The Complete Guide/Project/shared.js
@@ -4,6 +4,7 @@ const modalNoButton = document.querySelector('.modal__action--negative');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 // console.dir(backdrop);
 // console.dir(backdrop.style);
@@ -43,4 +44,16 @@ toggleButton.addEventListener('click', () => {
   mobileNav.classList.add('open');
   // mobileNav.style.display = "block";
   backdrop.classList.add('open');
+})
+
+ctaButton.addEventListener('animationstart', (event) => {
+  console.log('Animation started', event);
+})
+
+ctaButton.addEventListener('animationend', (event) => {
+  console.log('Animation ended', event);
+})
+
+ctaButton.addEventListener('animationiteration', (event) => {
+  console.log('Animation iteration', event);
 })
