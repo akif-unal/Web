a7a6b385dff7d1002e5b1ffea8d4271ed902095c
akifunal <akifunal05@gmail.com>
2021-03-20 18:55:26 +0300

    dom random background exercise

---
+++ Udemy/Zero to Mastery/Section 12 Dom/exercise-backgroundgenerator/script1.js
@@ -0,0 +1,38 @@
+const body = document.querySelector("body");
+const currentCss = document.querySelector("h2");
+const colorInput1 = document.querySelector(".color1");
+const colorInput2 = document.querySelector(".color2");
+const randomButton = document.querySelector(".random-button");
+
+// Prints background color on load
+currentCss.textContent =
+  getComputedStyle(body).getPropertyValue("background-image") + ";";
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function changeInputColors() {
+  colorInput1.value = "#" + getRandomColor();
+  colorInput2.value = "#" + getRandomColor();
+}
+
+function setBgText() {
+  currentCss.textContent = body.style.backgroundImage;
+}
+
+function setBgGradient() {
+  document.body.style.backgroundImage = `linear-gradient(to right, ${colorInput1.value}, ${colorInput2.value})`;
+  setBgText();
+}
+
+function changeBackground() {
+  changeInputColors();
+  setBgGradient();
+}
+
+colorInput1.addEventListener("input", setBgGradient);
+
+colorInput2.addEventListener("input", setBgGradient);
+
+randomButton.addEventListener("click", changeBackground);
