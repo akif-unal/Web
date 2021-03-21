9b72c404a1d80081d9bd9912a0b72eed3bb2925f
akifunal <akifunal05@gmail.com>
2021-03-21 23:51:23 +0300

    es6 exercise solved

--- Udemy/Zero to Mastery/Section 13 Advanced JavaScript/Exercise-3(ES5 and ES6)/exercise3.js
+++ Udemy/Zero to Mastery/Section 13 Advanced JavaScript/Exercise-3(ES5 and ES6)/exercise3.js
@@ -28,13 +28,6 @@ const okObj = {
 };
 
 // Template strings
-var message =
-  "Hello " +
-  firstName +
-  " have I met you before? I think we met in " +
-  city +
-  " last summer no???";
-
 const message = `Hello ${firstName} have I met you before? I think we met in ${city} last summer no???`;
 
 // default arguments
@@ -46,13 +39,5 @@ const isValidAge = (age = 10) => age;
 const sym = Symbol("This is my first Symbol");
 
 // Arrow functions
-function whereAmI(username, location) {
-  if (username && location) {
-    return "I am not lost";
-  } else {
-    return "I am totally lost!";
-  }
-}
-
 const whereAmI = (username, location) =>
   username && location ? "I am not lost" : "I am totally lost!";
